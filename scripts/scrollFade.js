bd52ceb056c914612f4fd9bad609c4ecce720f3f
LG <lg@LGs-MacBook-Pro.local>
2016-10-04 06:42:39 -0600

    Switched from using Bootstrap's affix to standard jQuery, new class called scrollFade

--- scripts/scrollFade.js
+++ scripts/scrollFade.js
@@ -3,12 +3,26 @@
 
     // set up scrolling when the DOM is ready
     $(document).ready(function() {
+        /*
+         * Scroll events can fire a lot. So only check the scrolling one time,
+         * 200ms after the scroll.
+         */
         $(window).scroll(function() {
-            if ($(this).scrollTop() > 50) {
-                $('.uspto-header-middle').fadeOut();
-            } else {
-                $('.uspto-header-middle').fadeIn();
+            var timeout;
+
+            if (!timeout) {
+                timeout = setTimeout(function () {
+                    clearTimeout(timeout);
+                    timeout = null;
+                    if ($(this).scrollTop() > 50) {
+                        $('#usptoGlobalHeader').addClass("scrollFade");
+                    } else {
+                        $('#usptoGlobalHeader').removeClass("scrollFade");
+                    } // if
+                }, 200);
             } // if
+
         });
+
     });
 })();
